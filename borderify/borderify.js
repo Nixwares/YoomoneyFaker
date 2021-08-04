abb40c955e8a65dd3e80c51fa334be2f4aeedf67
Saywex89 <omg12121@mail.ru>
2021-08-05 00:14:30 +0700

    add function FakePhone

--- borderify/borderify.js
+++ borderify/borderify.js
@@ -1,15 +1,32 @@
+const CHANGE_BALANCE = true;
+const CHANGE_PHONE = true;
 
-function changeStyles() {
-    const BALANCE = '25 200₽';
-    const BALANCE2 = '25 200₽';
-    
-    let balance_elem1 = document.getElementsByClassName('Text__StyledTextSpan-sc-9bqqn7-0 itslBl')[0];
-    let balance_elem2 = document.getElementsByClassName('styled__StyledPriceRub-sc-1jfqnx2-6 eWttka qa-rub')[0];
-    balance_elem1.innerHTML = BALANCE;
-    balance_elem2.innerHTML = BALANCE2;
-        
+
+function ChangePhone() {
+    const FakePhone = '+7900 0000000'
+    let NumberElem = document.getElementsByClassName('Text__StyledTextSpan-sc-9bqqn7-0 ibBqHn qa-favorite-example-phone')[0];
+    NumberElem.innerHTML = FakePhone;
 }
 
-  
 
-window.addEventListener("click",function() { changeStyles() });
+function ChangeVisibilityBalance() {
+    const BALANCE = '25 200₽';
+    const BALANCE2 = '25 200₽';
+    let BalanceElem1 = document.getElementsByClassName('Text__StyledTextSpan-sc-9bqqn7-0 itslBl')[0];
+    let BalanceElem2 = document.getElementsByClassName('styled__StyledPriceRub-sc-1jfqnx2-6 eWttka qa-rub')[0];
+    let BalanceElem3 = document.getElementsByClassName('Text__StyledTextSpan-sc-9bqqn7-0 fZqWsL')[0];
+
+    BalanceElem1.innerHTML = BALANCE;
+    BalanceElem2.innerHTML = BALANCE2;
+    BalanceElem3.innerHTML = BALANCE;
+}
+
+window.addEventListener("click",function() { 
+    if (CHANGE_BALANCE){
+        ChangeVisibilityBalance() 
+    }
+    if (CHANGE_PHONE) {
+        ChangePhone()
+
+    }
+});
