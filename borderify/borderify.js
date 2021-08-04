5d7c52f600626b41359f78517b4a7ce863120300
Saywex89 <omg12121@mail.ru>
2021-08-05 01:03:27 +0700

    improved styles

--- borderify/borderify.js
+++ borderify/borderify.js
@@ -1,5 +1,4 @@
-const CHANGE_BALANCE = true;
-const CHANGE_PHONE = true;
+const CHANCE_SCORES = true;
 
 
 function ChangePhone() {
@@ -10,23 +9,27 @@ function ChangePhone() {
 
 
 function ChangeVisibilityBalance() {
-    const BALANCE = '25 200₽';
-    const BALANCE2 = '25 200₽';
+    const BALANCE = '25 200';
+    const BALANCE2 = '25 200';
     let BalanceElem1 = document.getElementsByClassName('Text__StyledTextSpan-sc-9bqqn7-0 itslBl')[0];
     let BalanceElem2 = document.getElementsByClassName('styled__StyledPriceRub-sc-1jfqnx2-6 eWttka qa-rub')[0];
     let BalanceElem3 = document.getElementsByClassName('Text__StyledTextSpan-sc-9bqqn7-0 fZqWsL')[0];
 
-    BalanceElem1.innerHTML = BALANCE;
-    BalanceElem2.innerHTML = BALANCE2;
-    BalanceElem3.innerHTML = BALANCE;
+    BalanceElem1.innerHTML = `<b>${BALANCE}</b> ₽`;
+    BalanceElem2.innerHTML = `${BALANCE2} <small>₽</small>`;
+    BalanceElem3.innerHTML = `${BALANCE} ₽`;
 }
 
-window.addEventListener("click",function() { 
-    if (CHANGE_BALANCE){
-        ChangeVisibilityBalance() 
-    }
-    if (CHANGE_PHONE) {
-        ChangePhone()
+function ChangeCountScores() {
+    const FakeScores = '65';
+    let ScoresElem = document.getElementsByClassName('Text__StyledTextSpan-sc-9bqqn7-0 kQQMlI')[0];
+    ScoresElem.innerHTML = FakeScores;
 
-    }
+}
+
+window.addEventListener("click", function() { 
+    ChangePhone()
+    ChangeVisibilityBalance()
+    ChangeCountScores()
+
 });
